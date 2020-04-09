aade6f37c268652802969c8505e3191b0d2cae69
MrDeadLord <madeadlord@gmail.com>
2020-04-09 15:43:01 +0300

    s

---
+++ Windows Forms Lesson 2.1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Windows_Forms_2._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var working = new Work();
+
+            if (checkBox1.Checked)
+            {
+                foreach (var n in working.OpenAll())
+                {
+                    listBox1.Items.Add(n);
+                }
+            }       //Добавляем в список эле-ты из ВСЕХ дерикторий
+            else
+            {
+
+                foreach (var n in working.OprenFile())
+                {
+                    listBox1.Items.Add(n);
+                }
+            }                         //Добавляем ТОЛЬКО из выбранной
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                FileInfo fi = new FileInfo(listBox1.SelectedItem.ToString());
+
+                using (var fileStream = fi.OpenRead())
+                using (var sr = new StreamReader(fileStream))
+                {
+                    label1.Text = sr.ReadLine();
+                }
+            }                        
+        }
+    }
+}
---
+++ Windows Forms Lesson 2.1/Work.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Windows_Forms_2._1
+{
+    class Work
+    {
+        /// <summary>
+        /// Поиск файлов нужного формата
+        /// </summary>
+        /// <returns>Возвращает список файлом прошедших проверку</returns>
+        public List<object> OprenFile()
+        {
+            FolderBrowserDialog FolderBrowserDialog1 = new FolderBrowserDialog();
+            FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            if (FolderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo di = new DirectoryInfo(FolderBrowserDialog1.SelectedPath);
+                var files = di.GetFiles("*.docx");
+
+                var filesList = new List<object>();
+
+                foreach (var file in files)
+                {
+                    filesList.Add(file.FullName);
+                }
+
+                return filesList;
+            }
+            else
+            {
+                var emptyList = new List<object>();
+                return emptyList;
+            }
+        }
+
+        /// <summary>
+        /// Поиск нужных форматов, включая вложенные папки
+        /// </summary>
+        /// <returns>Возвращает список файлов прошедших проверку(формат)</returns>
+        public List<object> OpenAll()
+        {
+            FolderBrowserDialog FolderBrowserDialog1 = new FolderBrowserDialog();
+            FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+
+            if (FolderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryInfo di = new DirectoryInfo(FolderBrowserDialog1.SelectedPath);
+                var files = di.GetFiles("*.txt", SearchOption.AllDirectories);
+
+                var filesList = new List<object>();
+
+                foreach (var file in files)
+                {
+                    filesList.Add(file.FullName);
+                }
+
+                return filesList;
+            }
+            else
+            {
+                var emptyList = new List<object>();
+                return emptyList;
+            }
+        }
+
+    }
+}
